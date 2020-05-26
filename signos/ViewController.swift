657a4c710778aa7b18b948b3b43ffbe5e471aa39
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-26 16:35:50 -0300

    Initial Commit

---
+++ signos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signos
+//
+//  Created by Anderson Matuchenko on 26/05/20.
+//  Copyright © 2020 Anderson Matuchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
